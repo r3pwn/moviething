92727013a227c6e79e93c860bbca39baa694fa7a
Greg Willard (r3pwn) <wickett06@gmail.com>
2018-04-16 17:27:40 -0400

    add new urls for showings one and two

--- post.go
+++ post.go
@@ -6,7 +6,17 @@ import (
    "net/http"
 )
 
-var URL string = "https://docs.google.com/forms/d/e/1FAIpQLSdNpMBFYVMIOrcj8SixavtCdvuv7P3oGEPGSAoJYvH6J6CMgw/formResponse"
+var UrlShowOne string = "https://docs.google.com/forms/d/e/1FAIpQLScrLOwjzg8CL6EON0l2cSBzh6lLcgAJx3SHCLByHXw-mr65HA/formResponse"
+var FnameShowOne string = "entry.838312440"
+var LnameShowOne string = "entry.1393804301"
+var NumberShowOne string = "entry.1876171918"
+var EmailShowOne string = "entry.86023736"
+
+var UrlShowTwo string = "https://docs.google.com/forms/d/e/1FAIpQLSdmBqNXLUyJ95p9_6bTl8XaksyB54PLX59-775iAj_APn6e2w/formResponse"
+var FnameShowTwo string = "entry.2013781449"
+var LnameShowTwo string = "entry.1852780729"
+var NumberShowTwo string = "entry.2097210479"
+var EmailShowTwo string = "entry.1400159114"
 
 func Post(firstName, lastName, number, email string) {
    hc := http.Client{}
