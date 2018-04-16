eea71e5ea7b3105a7e1c4000f5a112a76ccd0be7
Greg Willard (r3pwn) <wickett06@gmail.com>
2018-04-16 19:38:46 -0400

    use new formassistant by @qwerty0981

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
     "strings"
     "time"
     "runtime"
+    "github.com/qwerty0981/formassistant"
 )
 
 func clear() {
@@ -39,7 +40,7 @@ func printHeader() {
     fmt.Println("")
 }
 
-func getStudentData(cardData string) {
+func getStudentData(cardData string) (fname, lname, number, email string) {
     // regex pattern to find last/first name
     nameReg, _ := regexp.Compile("\\^([a-zA-Z/]+)")
     name := nameReg.FindStringSubmatch(cardData)[1]
@@ -55,19 +56,37 @@ func getStudentData(cardData string) {
     email := string(nameSlice[1][0]) + nameSlice[0] + stuNum + "@floridapoly.edu"
     email = strings.ToLower(email)
     
-    Post(nameSlice[1], nameSlice[0], stuNum, email)
-    
-    fmt.Println("Swipe for " + nameSlice[1] + " submitted successfully...")
-    time.Sleep(time.Second)
+    return nameSlice[1], nameSlice[0], stuNum, email
 }
 
 func main() {
+    earlyShowing := form.GoogleForm("https://docs.google.com/forms/d/e/1FAIpQLScrLOwjzg8CL6EON0l2cSBzh6lLcgAJx3SHCLByHXw-mr65HA/formResponse")
+    lateShowing := form.GoogleForm("https://docs.google.com/forms/d/e/1FAIpQLSdmBqNXLUyJ95p9_6bTl8XaksyB54PLX59-775iAj_APn6e2w/formResponse")
+
+    earlyShowing.AddEndpoint("firstName", "entry.838312440")
+    earlyShowing.AddEndpoint("lastName", "entry.1393804301")
+    earlyShowing.AddEndpoint("email", "entry.86023736")
+    earlyShowing.AddEndpoint("number", "entry.1876171918")
+
+    lateShowing.AddEndpoint("firstName", "entry.2013781449")
+    lateShowing.AddEndpoint("lastName", "entry.1852780729")
+    lateShowing.AddEndpoint("email", "entry.1400159114")
+    lateShowing.AddEndpoint("number", "entry.2097210479")
+
     reader := bufio.NewReader(os.Stdin)
     for {
         printHeader()
         fmt.Println("Please swipe a card...")
         cardData, _ := reader.ReadString('\n')
-        getStudentData(string(cardData))
+        fname, lname, num, email := getStudentData(string(cardData))
+        values := make(map[string]string)
+        values["firstName"] = fname
+        values["lastName"] = lname
+        values["number"] = num
+        values["email"] = email
+        earlyShowing.Post(values)
+        fmt.Println("Swipe for " + fname + " submitted successfully...")
+        time.Sleep(time.Second)
         clear()
     }
 }
